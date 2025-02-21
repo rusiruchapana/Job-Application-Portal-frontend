58ce7e5b7c671cbcff094f9f58a1badaca2c0d89
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-21 11:54:03 +0530

    job list page

---
+++ job-application-portal/src/app/pages/job-list/job-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-list',
+  imports: [],
+  templateUrl: './job-list.component.html',
+  styleUrl: './job-list.component.css'
+})
+export class JobListComponent {
+
+}
