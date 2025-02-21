8862baa257ed76d0e0a6bc6055a1dc2afc6ea676
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-21 11:52:59 +0530

    job apply page

---
+++ job-application-portal/src/app/pages/job-apply/job-apply.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-apply',
+  imports: [],
+  templateUrl: './job-apply.component.html',
+  styleUrl: './job-apply.component.css'
+})
+export class JobApplyComponent {
+
+}
