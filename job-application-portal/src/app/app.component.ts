471df7f3e993f019b0c6b65543de605ef359baf3
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-22 10:24:39 +0530

    #

--- job-application-portal/src/app/app.component.ts
+++ job-application-portal/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JobApplyComponent } from "./pages/job-apply/job-apply.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,  JobApplyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
