76acb8bcb319e5ac4242917e71f5f79ed5fd9fad
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-23 13:09:14 +0530

    configure routing

--- job-application-portal/src/app/app.component.ts
+++ job-application-portal/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { JobApplyComponent } from "./pages/job-apply/job-apply.component";
-import { JobListComponent } from "./pages/job-list/job-list.component";
-import { AdminDashboardComponent } from "./pages/admin-dashboard/admin-dashboard.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, JobApplyComponent, JobListComponent, AdminDashboardComponent],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [RouterModule]
 })
 export class AppComponent {
   title = 'job-application-portal';
--- job-application-portal/src/app/app.routes.ts
+++ job-application-portal/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { JobListComponent } from './pages/job-list/job-list.component';
+import { JobDetailsComponent } from './pages/job-details/job-details.component';
+import { JobApplyComponent } from './pages/job-apply/job-apply.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: JobListComponent},
+    {path: 'job/:id' , component: JobDetailsComponent},
+    {path: 'apply/:id' , component: JobApplyComponent},
+    {
+        path: 'admin',
+        loadComponent: ()=> import('./pages/admin-dashboard/admin-dashboard.component')
+        .then(m=> m.AdminDashboardComponent)
+    }
+];
