5d062d15b8f52b4037cde287b1704b03832ba1aa
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-23 11:05:57 +0530

    joblisting is complete

--- job-application-portal/src/app/app.component.ts
+++ job-application-portal/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { JobApplyComponent } from "./pages/job-apply/job-apply.component";
+import { JobListComponent } from "./pages/job-list/job-list.component";
+import { AdminDashboardComponent } from "./pages/admin-dashboard/admin-dashboard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,  JobApplyComponent],
+  imports: [RouterOutlet, JobApplyComponent, JobListComponent, AdminDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- job-application-portal/src/app/pages/job-list/job-list.component.ts
+++ job-application-portal/src/app/pages/job-list/job-list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JobService } from '../../services/job.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-job-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './job-list.component.html',
   styleUrl: './job-list.component.css'
 })
-export class JobListComponent {
+export class JobListComponent implements OnInit {
+
+  jobs: any[] = [];
+
+  constructor(private jobService: JobService) { }
+
+  ngOnInit(): void {
+    this.jobService.getJobs().subscribe(jobs => {
+      this.jobs = jobs;
+    });
+  }
+
+  logJob(job: any): void {
+    console.log(job);
+  }
 
 }
